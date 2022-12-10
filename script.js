29fc1cc96d19986d5367fc562e86e4f00e41456e
Gabriel Rosinski <113986687+Gabrx7@users.noreply.github.com>
2022-12-10 10:53:10 -0300

    Edição

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+function Grupo(){
+    window.location.href = ''
+}
+
 function ComprarRifa(){
     window.location.href = 'https://api.whatsapp.com/send?phone=5544997619331&text=Ol%C3%A1%20Gabr!%20Queria%20adquirir%20a%20rifa%20de%20numero%20#01,%20O%20numero%20que%20desejo%20comprar%20%C3%A9:%20%22%20%20%22'
 };
